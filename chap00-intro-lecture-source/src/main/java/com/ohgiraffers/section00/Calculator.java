ed642c06f0dec024aa4982d380a2624d84d01e0e
yanghyeyeon <yhy4058@gmail.com>
2024-07-14 20:06:52 +0900

    intro calculator

---
+++ chap00-intro-lecture-source/src/main/java/com/ohgiraffers/section00/Calculator.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.section00;
+
+public class Calculator {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+}
