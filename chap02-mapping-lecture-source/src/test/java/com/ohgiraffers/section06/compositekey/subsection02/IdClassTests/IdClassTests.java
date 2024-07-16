b7fbeadfe115d762ce6beb127b8e1c3109c3df63
yanghyeyeon <yhy4058@gmail.com>
2024-07-16 10:51:45 +0900

    mapping compositekey idclasstests

--- chap02-mapping-lecture-source/src/test/java/com/ohgiraffers/section06/compositekey/subsection02/IdClassTests/IdClassTests.java
+++ chap02-mapping-lecture-source/src/test/java/com/ohgiraffers/section06/compositekey/subsection02/IdClassTests/IdClassTests.java
@@ -9,45 +9,45 @@ import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class IdClassTests {
-
+    
     private static EntityManagerFactory entityManagerFactory;
     private static EntityManager entityManager;
-
+    
     @BeforeAll
     public static void initFactory() {
         entityManagerFactory = Persistence.createEntityManagerFactory("jpatest");
     }
-
+    
     @BeforeEach
     public void initManager() {
         entityManager = entityManagerFactory.createEntityManager();
     }
-
+    
     @AfterAll
     public static void closeFactory() {
         entityManagerFactory.close();
     }
-
+    
     @AfterEach
     public void closeManager() {
         entityManager.close();
     }
-
+    
     @DisplayName("아이디 클래스를 사용한 복합키 테이블 매핑 테스트")
     @Test
     public void embeddedIdTest() {
-
+        
         // given
         Member member = new Member();
         member.setMemberNo(1);
         member.setMemberId("user01");
         member.setPhone("010-1234-5678");
         member.setAddress("서울시 삼성동");
-
+        
         // when
         EntityTransaction transaction = entityManager.getTransaction();
         transaction.begin();
-
+        
         try {
             entityManager.persist(member);
             transaction.commit();
@@ -55,7 +55,7 @@ public class IdClassTests {
             transaction.rollback();
             throw new RuntimeException(e);
         }
-
+        
         // then
         Member foundMember = entityManager.find(Member.class, new MemberPK(1,"user01"));
         
