a4ab8e431283b8ef94b7380535a5f2fa171b68cf
yanghyeyeon <yhy4058@gmail.com>
2024-07-18 14:31:55 +0900

    spring data hpa menu model entity

---
+++ chap05-spring-data-jpa-lecture-source/src/main/java/com/ohgiraffers/springdatajpa/menu/model/entity/Menu.java
@@ -0,0 +1,32 @@
+package com.ohgiraffers.springdatajpa.menu.model.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "tbl_menu")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Menu {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "menu_code")
+    private int menuCode;
+
+    @Column(name = "menu_name")
+    private String menuName;
+
+    @Column(name = "menu_price")
+    private int menuPrice;
+
+    @Column(name = "category_code")
+    private int categoryCode;
+
+    @Column(name = "orderable_status")
+    private String orderableStatus;
+
+}
