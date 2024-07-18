ab1b76f45ef45fd683c1f454e4ec2857b664c34a
yanghyeyeon <yhy4058@gmail.com>
2024-07-18 14:35:14 +0900

    spring data jpa menu controller

---
+++ chap05-spring-data-jpa-lecture-source/src/main/java/com/ohgiraffers/springdatajpa/menu/controller/MenuController.java
@@ -0,0 +1,104 @@
+package com.ohgiraffers.springdatajpa.menu.controller;
+
+import com.ohgiraffers.springdatajpa.common.Pagenation;
+import com.ohgiraffers.springdatajpa.common.PagingButtonInfo;
+import com.ohgiraffers.springdatajpa.menu.model.dto.CategoryDto;
+import com.ohgiraffers.springdatajpa.menu.model.dto.MenuDto;
+import com.ohgiraffers.springdatajpa.menu.model.service.MenuService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Slf4j
+@Controller
+@RequestMapping("/menu")
+public class MenuController {
+
+    private final MenuService menuService;
+
+    public MenuController(MenuService menuService){
+        this.menuService = menuService;
+    }
+
+    // 메뉴 단일 조회 기능
+    @GetMapping("/{menuCode}")
+    public String findMenuByCode(@PathVariable("menuCode") int menuCode, Model model) {
+
+        log.info("menuCode = {}", menuCode);
+
+       MenuDto menu = menuService.findMenuByCode(menuCode);
+
+       model.addAttribute("menu", menu);
+
+        return "menu/detail";
+    }
+/*페이징 처리 전*/
+//    @GetMapping("/list")
+//    public String findMenuList(Model model) {
+//        List<MenuDto> menuList = menuService.findMenuList();
+//
+//        model.addAttribute("menuList",menuList);
+//
+//        return "menu/List";
+//    }
+
+    @GetMapping("/list")
+    public String findAllMenus(@PageableDefault Pageable pageable, Model model) {
+
+        log.info("pageable = {}" , pageable);
+
+        Page<MenuDto> menuList = menuService.findAllMenus(pageable);
+
+        log.info("조회한 내용 목록 : {}", menuList.getContent());
+        log.info("총 페이지 수 : {}", menuList.getTotalPages());
+        log.info("총 메뉴 수 : {}", menuList.getTotalElements());
+        log.info("해당 페이지에 표시 될 요소 수 : {}", menuList.getSize());
+        log.info("해당 페이지에 실제 요소 수 : {}", menuList.getNumberOfElements());
+        log.info("첫 페이지 여부 : {}", menuList.isFirst());
+        log.info("마지막 페이지 여부 : {}", menuList.isLast());
+        log.info("정렬 방식 : {}", menuList.getSort());
+        log.info("여러 페이지 중 현재 인덱스 : {}", menuList.getNumber());
+
+        PagingButtonInfo paging = Pagenation.getPagingButtonInfo(menuList);
+
+        model.addAttribute("paging", paging);
+        model.addAttribute("menuList", menuList);
+
+        return "menu/list";
+    }
+
+    @GetMapping("/querymethod")
+    public void queryMethodPage() {}
+
+    @GetMapping("/search")
+    public String findByMenuPrice(@RequestParam Integer menuPrice , Model model) {
+
+        log.info("menuPrice =================== {}", menuPrice);
+
+        List<MenuDto> menuList = menuService.findByMenuPrice(menuPrice);
+
+        model.addAttribute("menuList", menuList);
+        model.addAttribute("menuPrice",menuPrice);
+
+        return "menu/searchResult";
+    }
+
+    @GetMapping("/regist")
+    public void registPage() {}
+
+//    @GetMapping(value = "category", produces = "application/json; charset=utf-8")
+//    @ResponseBody
+//    public List<CategoryDto> categoryPage() {
+//
+//        List<CategoryDto> categoryList  = menuService.findAllCategory();
+//        log.info("categoryList ================= {}",categoryList);
+//
+//        return categoryList;
+//    }
+}
